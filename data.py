7b3e98f213a97c375ddd5d4c53a5f418023ae8d2
Ufuk Kirmizigedik <67425232+ufukkirmizigedik@users.noreply.github.com>
2024-08-11 02:11:35 +0300

    Update data.py

--- data.py
+++ data.py
@@ -3,8 +3,8 @@ import json
 data = {
     "name" : "rshvspsutlbr eaer",
     "adres" : "jsnhjdbnzyjdf 5r2 rs 66",
-    "login" : "89251771507",
-    "password" : "Ufuk1478963"
+    "login" : "Login",
+    "password" : "Password"
 }
 json_object = json.dumps(data,indent=4)
 with open("data.json","w") as clients:
